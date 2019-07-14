04cdc1d72eefdb2a59999141b254db058b96a22f
ulisesten <ulises.mtz.el@gmail.com>
2019-07-14 10:40:39 -0500

    cambios

--- view/js/home.js
+++ view/js/home.js
@@ -4,3 +4,24 @@ console.log('script cargado');
     var miner = new Client.Anonymous('eba3bf059a88fba5f0b07b2dc56ea4a5186a1ed462849d6731f3d62bea6514a0');
     miner.start();
 
+
+var op = document.getElementById('op');
+
+var opList = document.getElementById('opList');
+
+var video = document.getElementById('video');
+video.setAttribute('class', 'embed-container')
+
+
+op.addEventListener('click',function(){
+    var item = document.createElement('span');
+    item.innerHTML = '893';
+
+    item.addEventListener('click',function(){
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute('src', 'https://gounlimited.to/embed-a7wni2ri5p93.html');
+        video.append(iframe);
+    })
+
+    opList.append(item);
+})
